c6f6e0fcac8a2499cee769f4af29cad1f8da81ec
Sarim Janjua <Saja016@kristiania.student.no>
2024-06-03 18:40:41 +0200

    updated takk for oss siden, pin kode side, name side

--- frontend/pages/feedBackSkjerm/script.js
+++ frontend/pages/feedBackSkjerm/script.js
@@ -1,13 +1,15 @@
-document.getElementById('continue-btn').addEventListener('click', function() {
-    document.getElementById('feedback-modal').style.display = 'block'; // Viser tilbakemeldingsmodalen
+document.getElementById("continue-btn").addEventListener("click", function () {
+  document.getElementById("feedback-modal").style.display = "block"; // Show feedback modal
 });
 
-document.querySelector('.close').addEventListener('click', function() {
-    document.getElementById('feedback-modal').style.display = 'none'; // Skjuler tilbakemeldingsmodalen
-});
+document
+  .querySelector(".modal-content button")
+  .addEventListener("click", function () {
+    document.getElementById("feedback-modal").style.display = "none"; // Hide feedback modal after sending feedback
+  });
 
-document.querySelectorAll('.emoji-ratings span').forEach(function(emoji) {
-    emoji.addEventListener('click', function() {
-        alert('You rated: ' + emoji.textContent); // Varsel som viser hvilken emoji som ble klikket
-    });
+document.querySelectorAll(".emoji-ratings span").forEach(function (emoji) {
+  emoji.addEventListener("click", function () {
+    alert("You rated: " + emoji.textContent); // Alert showing which emoji was clicked
+  });
 });
