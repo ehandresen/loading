2e07849aca090b0ef5e53d91b5fccd5b6a13c4f5
ViSaLearning <59650033+ViSaLearning@users.noreply.github.com>
2024-06-04 23:50:33 +0200

    Update multichoise.js

--- frontend/pages/multichoise/multichoise.js
+++ frontend/pages/multichoise/multichoise.js
@@ -1,3 +1,4 @@
+const url = 'http://localhost:3000/get-values';
 var Button1 = document.getElementById("answer1");
 var Button2 = document.getElementById("answer2");
 var Button3 = document.getElementById("answer3");
@@ -6,13 +7,32 @@ var QuestionBody = document.getElementById("theQuestion");
 var questions;
 var quest, An1, An2, An3, An4;
 
+
+
 function getData() {
-    quest = "Q";
-    An1 = "a";
-    An2 = "b";
-    An3 = "c";
-    An4 = "d";
-    updateFront();
+    fetch(url)
+    .then(response => {
+        // Sjekke om responsen er vellykket (statuskode 200-299)
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        // Konvertere responsen til JSON
+        return response.json();
+    })
+    .then(data => {
+        // Logge dataen for debugging
+        quest = data.question;
+        An1 = data.answerA;
+        An2 = data.answerB;
+        An3 = data.answerC;
+        An4 = data.answerD;
+        console.log(data);
+        updateFront();
+    })
+    .catch(error => {
+        // Håndtere eventuelle feil som oppstår under forespørselen
+        console.error('There was a problem with the fetch operation:', error);
+    });
 }
 
 function updateFront(){
@@ -28,7 +48,7 @@ function updateFront(){
 }
 
 function Timer(){
-    var fullCounter = 30
+    var fullCounter = 500
     var counter = document.getElementById("timer")
     function countdown() {
         counter.innerHTML = `${fullCounter}`;
@@ -64,7 +84,7 @@ function choose4(){
 }
 
 Timer();
-//getData();
+getData();
 Button1.onclick = choose1;
 Button2.onclick = choose2;
 Button3.onclick = choose3;
