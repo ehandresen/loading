eb204424007144e97fde9ced18c713501c758861
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-06-14 10:29:38 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,13 @@
+// this was a test database setup to try to query data from mysql
+
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = mysql
   .createPool({
-    // legg inn riktig data i filen .env
+    // legg inn riktig data i '.env' filen
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
@@ -10,10 +15,22 @@ const pool = mysql
   })
   .promise();
 
-async function getNotes() {
-  const [rows] = await pool.query('select * from notes');
+export async function getNotes() {
+  const [rows] = await pool.query('select * from feedback');
   return rows;
 }
-const notes = await getNotes();
 
-console.log(notes);
+export async function getNote(id) {
+  const [rows] = await pool.query('select * from notes where id = ?', [id]);
+  return rows[0];
+}
+
+export async function createNote(titile, content) {
+  const [result] = await pool.query(
+    'insert into notes (titile, contents) values (?, ?)',
+    [titile, content]
+  );
+  const id = result.insertId;
+  return getNote(id);
+}
+
